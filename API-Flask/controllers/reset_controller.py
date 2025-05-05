4e2c1a45aa96256646325134fc384fc3064c641e
Henry Modesto <modestohenry48@gmail.com>
2025-05-05 00:26:26 -0300

    FETCH(PASTA CORRETA)

--- API-Flask/controllers/reset_controller.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-reset_bp = Blueprint('reset', __name__)
-
-@reset_bp.route('/reset', methods=['POST'])
-def reset():
-    return {"mensagem": "Banco de dados resetado com sucesso!"}, 200
